3dfacbb2846d8bf3dde273212d53b0f55bd7eb29
Augustus Boling <geekish2428@gmail.com>
2017-04-04 18:27:24 -0700

    Added new login page, fixed compatibility issues, now renders properly

--- main.py
+++ main.py
@@ -25,9 +25,8 @@ env = jinja2.Environment(loader = jinja2.FileSystemLoader(os.path.dirname(__file
 
 class Login(webapp2.RequestHandler):
     def get(self):
-        template = env.get_template('/html/mainPage.html')
+        template = env.get_template('/html/bootstrap_login.html')
         self.response.out.write(template.render())
-
     def post(self):
         pass
 
